792488132849b57118675289af6340066fcb2ead
minhtuanbui <89298354+minhtuanbui@users.noreply.github.com>
2021-08-22 16:45:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const wordpressPanel = document.querySelector(".panels-container .wordpress");
+const bloggerPanel  = document.querySelector(".panels-container .blogger");
+const wixPanel  = document.querySelector(".panels-container .wix");
+
+const wordpressBtn = document.querySelector(".tabs-container .wordpress-btn");
+const bloggerBtn = document.querySelector(".tabs-container .blogger-btn");
+const bloggerBtn = document.querySelector(".tabs-container .wix-btn");
